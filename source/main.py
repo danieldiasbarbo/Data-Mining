f5f087b08576d76abfdd644fa32b72f55c05d44f
Daniel Dias Barbosa <danieldiasbarbo@gmail.com>
2023-06-10 15:27:44 -0300

    first try to compare classification algorithms

--- source/main.py
+++ source/main.py
@@ -1,8 +1,18 @@
 import pandas as pd
-import os.path
+import os
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.naive_bayes import GaussianNB
 
-if __name__ == "__main__":
-    diretorio = os.path.join(os.path.dirname(__file__), "../data/ZP-River.dat")
+path = "../data/ZP-River.dat"
+
+
+def load_data(path):
+    diretorio = os.path.join(os.path.dirname(__file__), path)
 
     nomes = [
         "POSITION",
@@ -21,8 +31,54 @@ if __name__ == "__main__":
         "ACTION_HERO",
     ]
 
-    dados = pd.read_table(
+    return pd.read_table(
         filepath_or_buffer=diretorio, header=None, names=nomes, sep=" "
     )
 
-    print(dados.head(10))
+
+def classificar(i_tr, i_te, o_tr, o_te, clas):
+    clas.fit(i_tr, o_tr)
+    predicao = clas.predict(i_te)
+    acuracia = accuracy_score(output_teste, predicao)
+    print(type(clas).__name__, " Acc = ", acuracia)
+
+
+if __name__ == "__main__":
+    dados = load_data(path)
+    input = dados.drop("ACTION_HERO", axis=1)
+    output = dados.ACTION_HERO
+
+    input_treino, input_teste, output_treino, output_teste = train_test_split(
+        input, output, train_size=2 / 3
+    )
+
+    classificar(
+        input_treino,
+        input_teste,
+        output_treino,
+        output_teste,
+        KNeighborsClassifier(n_neighbors=5),
+    )
+
+    classificar(input_treino, input_teste, output_treino, output_teste, SVC())
+
+    classificar(
+        input_treino, input_teste, output_treino, output_teste, DecisionTreeClassifier()
+    )
+
+    classificar(
+        input_treino,
+        input_teste,
+        output_treino,
+        output_teste,
+        MLPClassifier(
+            solver="lbfgs",
+            alpha=1e-5,
+            hidden_layer_sizes=(50, 20),
+            random_state=1,
+            max_iter=10,
+            verbose=False,
+        ),
+    )
+
+    classificar(input_treino, input_teste, output_treino, output_teste, GaussianNB())
